447457911e5ae4ccec3fcac5d6c6f25b3e1e00bc
Pierre Dupont <pierre.dupont@nmbu.no>
2024-11-05 09:05:07 +0100

    PD

--- R/cleanRovBaseData_bear.R
+++ R/cleanRovBaseData_bear.R
@@ -40,23 +40,154 @@
 NULL
 #' @rdname cleanRovbaseData_bear
 #' @export
-cleanRovbaseData_bear <- function(
+cleanRovbaseData <- function(
+    species = c("bear","wolf","wolverine"),
     years = NULL, 
     samplingMonths = NULL,
+    renamingList = c( 
+      Age_estimated = "Alder, vurdert",
+      Age = "Alder, verifisert",
+      Age_verif_by = "Alder, verifisert av",
+      Age_class = "Alder på dødt individ",
+      Age_class_verif = "Aldersklasse verifisert SVA",
+      Analyzed_by = "AnalysertAv",
+      Analysis_priority = "Analyseprioritet",
+      Approved_by = "Godkjent av",
+      Approved_date = "Godkjentdato",
+      Assessment = "Vurdering",
+      Barcode_sample = "Strekkode (Prøve)",
+      Barcode = "Strekkode (Analyse)",
+      Birth_territory = "Født revir",  
+      CITES = "CITES-nummer",
+      Collected_by = "Hvem samlet inn",
+      Collector_name = "Samlet selv - Navn",
+      Collector_phone = "Samlet selv - Telefon", 
+      Collector_email = "Samlet selv - E-post",
+      Collector_role = "Samlet selv - Rolle",
+      Collector_other_name = "Annen innsamler - Navn" ,
+      Collector_other_phone = "Annen innsamler - Telefon", 
+      Collector_other_email = "Annen innsamler - E-post",
+      Collector_other_role = "Annen innsamler - Rolle",
+      Comments_sample = "Merknad (Prøve)",
+      Comments = "Merknad (Analyse)",
+      Control_status = "Kontrollstatus",
+      Coordinate_system = "Koordinatsystem",
+      Counted_off_against_decision = "Regnes av mot vedtak", 
+      County_number = "Fylkenummer",
+      County = "Fylke",
+      Date = "Funnetdato",
+      Date = "Dødsdato",
+      Death_cause = "Bakgrunn/årsak",
+      Death_method = "Bakgrunn/årsak metode",
+      Death_purpose = "Bakgrunn/årsak formål", 
+      DNAID_sample = "DNAID (Prøve)",
+      DNAID = "DNAID (Analyse)",
+      EventID = "HendelseID",
+      East_Original = "Øst (opprinnelig)",
+      East_RT90 = "Øst (RT90)",
+      East_UTM33 = "Øst (UTM33/SWEREF99 TM)",
+      Felling_site_verif = "Kontroll av fellingsted",
+      Field_personnel ="Feltpersonell",
+      Hunting_date = "Observasjons/Jaktdato",
+      Id = "Individ", 
+      Juvenile = "Yngling",
+      Mountain_area = "Fjellområde",
+      Method = "Metode",
+      Municipality_number = "Kommunenummer",
+      Municipality = "Kommune", 
+      North_original = "Nord (opprinnelig)",
+      North_RT90 = "Nord (RT90)",
+      North_UTM33 = "Nord (UTM33/SWEREF99 TM)",
+      Origin = "Opprinnelse",
+      Outcome = "Utfall", 
+      Last_saved_by_sample = "Sist lagret av (Prøve)",
+      Last_saved_sample = "Sist lagret dato (Prøve)",
+      Last_saved_by = "Sist lagret av (Analyse)",
+      Last_saved = "Sist lagret dato (Analyse)",
+      Last_saved_by = "Sist lagret av",
+      Last_saved =  "Sist lagret dato",
+      Locality = "Lokalitet",
+      Location = "Funnsted",
+      Lansstyrelsen_number = "Länsstyrelsens nr",
+      Quality_checked = "Kvalitetssikret av feltpersonell",
+      Quality_check_name = "Kvalitetssikrer - navn",
+      Quality_check_orga = "Kvalitetssikrer - Organisasjon",
+      Release_Date = "Frigivelsesdato", 
+      Sample_type = "Prøvetype", 
+      Sensitivity = "Følsomhet",
+      Species_sample = "Art (Prøve)",
+      Site_quality = "Stedkvalitet",
+      Time_of_death = "Dødstidspunkt",
+      Tips_name = "Tipser - Navn",
+      Tips_phone = "Tipser - Telefon",
+      Tips_email = "Tipser - E-post",
+      Tips_role = "Tipser - Rolle",
+      Tissue_sample = "Vevsprøve tatt",
+      Release_Date = "Frigivelsesdato", 
+      RovbaseID = "RovbaseID (Analyse)",
+      RovbaseID_sample = "RovbaseID (Prøve)",
+      Species = "Art (Analyse)",
+      Species = "Art",
+      Sample_status = "Prøvestatus",
+      Sensitivity = "Følsomhet",
+      Sex_analysis = "Kjønn (Analyse)",
+      Sex = "Kjønn (Individ)",
+      Sex = "Kjønn",
+      Site_quality = "Stedkvalitet",
+      SVAID = "SVAID",
+      Uncertain_date = "Usikker dødsdato",
+      Weight_slaughter = "Slaktevekt",
+      Weight_total =  "Helvekt"),
     data_dir = "./Data",
     output_dir = "./Data",
     Rmd_template = NULL,
     overwrite = FALSE){
   
   ##----- 0. INITIAL CHECKS -----
+  
+  ##-- Species
+  if(sum(grep("bear", species, ignore.case = T))>0|sum(grep("bjorn", species, ignore.case = T))>0){
+    engSpecies <- "bear"
+    norSpecies <- "Bjørn"
+  }
+  if(sum(grep("wolf", species, ignore.case = T))>0|sum(grep("ulv", species, ignore.case = T))>0){
+    engSpecies <- "wolf"
+    norSpecies <- "Ulv"
+  }
+  if(sum(grep("wolverine", species, ignore.case = T))>0|sum(grep("jerv", species, ignore.case = T))>0){
+    engSpecies <- "wolverine"
+    norSpecies <- "Jerv"
+  }
+  
+  
+  ##-- Years
+  years <- if(is.null(years)){ years <- 2012:as.numeric(format(Sys.Date(), "%Y")) }
+  
+  
+  ##-- Sampling months
+  if(is.null(samplingMonths)){
+    if(engSpecies <- "bear"){
+      samplingMonths <- list(4:11)
+    }
+    if(engSpecies <- "wolf"){
+      samplingMonths <- list(c(10:12),c(1:4))
+    }
+    if(engSpecies <- "wolverine"){
+      samplingMonths <- list(c(10:12),c(1:4))
+    } else {
+      stop("No default setting available for the monitoring period of this species. \n You must specify the monitoring season months through the 'samplingMonths' argument.")
+    }
+  }
+  
+
   ##-- Extract the date from the last .xlsx data file
   DATE <- getMostRecent( path = data_dir, pattern = "DNA.xlsx")
   
   ##-- Get file name for clean data
-  fileName <- paste0("Data_Bear_", DATE,".RData")
+  fileName <- paste0("Data_",engSpecies,"_", DATE,".RData")
   
   ##-- Check that a file with that name does not already exist to avoid overwriting
-  if(file.exists(file.path(output_dir, fileName))){
+  if(any(file.exists(file.path(output_dir, fileName)))){
     message(paste0("A file named '",fileName,"' already exists in the specified directory."))
     message("Are you sure you want to proceed and overwrite existing clean data? (y/n) ")
     question1 <- readLines(n = 1)
@@ -65,9 +196,11 @@ cleanRovbaseData_bear <- function(
       overwrite <- FALSE
       return(overwrite)
     } else {
-      message(paste0("Now overwriting 'Data_", SPECIES, "_", DATE,".html'."))
+      message(paste0("Now overwriting '", fileName,"'."))
     }
   }
+  
+  
   
   ##----- 1. LOAD RAW NGS DATA -----
   ##-- Load raw excel file imported from rovbase 
@@ -77,63 +210,7 @@ cleanRovbaseData_bear <- function(
     ##-- Remove any duplicates
     distinct(., .keep_all = TRUE) %>%
     ##-- Rename columns to facilitate manipulation
-    rename(., any_of(c( DNAID_sample = "DNAID (Prøve)",
-                        Barcode_sample = "Strekkode (Prøve)",
-                        RovbaseID_sample = "RovbaseID (Prøve)",
-                        EventID = "HendelseID",
-                        Analysis_priority = "Analyseprioritet",
-                        Species_sample = "Art (Prøve)",
-                        Sample_type = "Prøvetype", 
-                        Date = "Funnetdato",
-                        Mountain_area = "Fjellområde",
-                        Sensitivity = "Følsomhet",
-                        Release_Date = "Frigivelsesdato", 
-                        Locality = "Lokalitet",
-                        Location = "Funnsted",
-                        North_original = "Nord (opprinnelig)",
-                        East_Original = "Øst (opprinnelig)",
-                        North_UTM33 = "Nord (UTM33/SWEREF99 TM)",
-                        East_UTM33 = "Øst (UTM33/SWEREF99 TM)" ,
-                        North_RT90 = "Nord (RT90)",
-                        East_RT90 = "Øst (RT90)" ,
-                        Coordinate_system = "Koordinatsystem" ,
-                        Site_quality = "Stedkvalitet",
-                        Collected_by = "Hvem samlet inn",
-                        Collector_name = "Samlet selv - Navn",
-                        Collector_phone = "Samlet selv - Telefon", 
-                        Collector_email = "Samlet selv - E-post",
-                        Collector_role = "Samlet selv - Rolle",
-                        Collector_other_name = "Annen innsamler - Navn" ,
-                        Collector_other_phone = "Annen innsamler - Telefon", 
-                        Collector_other_email = "Annen innsamler - E-post",
-                        Collector_other_role = "Annen innsamler - Rolle",
-                        Tips_name = "Tipser - Navn",
-                        Tips_phone = "Tipser - Telefon",
-                        Tips_email = "Tipser - E-post",
-                        Tips_role = "Tipser - Rolle",
-                        Quality_checked = "Kvalitetssikret av feltpersonell",
-                        Quality_check_name = "Kvalitetssikrer - navn",
-                        Quality_check_orga = "Kvalitetssikrer - Organisasjon",
-                        Comments_sample = "Merknad (Prøve)",
-                        Last_saved_by_sample = "Sist lagret av (Prøve)",
-                        Last_saved_sample = "Sist lagret dato (Prøve)",
-                        DNAID = "DNAID (Analyse)",
-                        Barcode = "Strekkode (Analyse)",
-                        RovbaseID = "RovbaseID (Analyse)",
-                        Species = "Art (Analyse)",
-                        Sample_status = "Prøvestatus",
-                        Id = "Individ",
-                        Sex_analysis = "Kjønn (Analyse)",
-                        Sex = "Kjønn (Individ)",
-                        Method = "Metode",
-                        Analyzed_by = "AnalysertAv",
-                        Comments = "Merknad (Analyse)",
-                        Last_saved_by = "Sist lagret av (Analyse)" ,
-                        Last_saved = "Sist lagret dato (Analyse)" ,
-                        Municipality_number = "Kommunenummer",
-                        Municipality = "Kommune", 
-                        County_number = "Fylkenummer",
-                        County = "Fylke"))) %>%
+    rename(., any_of(renamingList)) %>%
     ##-- Add some columns
     dplyr::mutate( 
       ##-- Add "Country" column
@@ -147,9 +224,9 @@ cleanRovbaseData_bear <- function(
   ##-- Filter data
   filter(.,
          ##-- Filter to the focal species
-         Species %in% "Bjørn",
+         Species %in% c(norSpecies),
          ##-- Filter out samples with 
-         Year %in% years) 
+         Year %in% c(years)) 
   
   ##############################################################################
   numDetDNA <- nrow(DNA)              ## Number of NGS samples in Rovbase
@@ -168,69 +245,23 @@ cleanRovbaseData_bear <- function(
     ##-- Remove any duplicates
     distinct(., .keep_all = TRUE) %>%
     ##-- Rename columns to facilitate manipulation
-    rename(., any_of(c( Species = "Art",
-                        Death_cause = "Bakgrunn/årsak",
-                        Death_method = "Bakgrunn/årsak metode",
-                        Death_purpose = "Bakgrunn/årsak formål", 
-                        Date = "Dødsdato",
-                        Uncertain_date = "Usikker dødsdato",
-                        Time_of_death = "Dødstidspunkt",
-                        Age_class = "Alder på dødt individ",
-                        Age_class_verif = "Aldersklasse verifisert SVA",
-                        Juvenile = "Yngling",
-                        Sex = "Kjønn", 
-                        Age_estimated = "Alder, vurdert",
-                        Age = "Alder, verifisert",
-                        Age_verif_by = "Alder, verifisert av",
-                        Full_weight =  "Helvekt",
-                        Slaughter_weight =  "Slaktevekt",
-                        CITES = "CITES-nummer",
-                        Felling_site_verif = "Kontroll av fellingsted",
-                        Tissue_sample = "Vevsprøve tatt",
-                        Hunting_date = "Observasjons/Jaktdato",
-                        Field_personnel ="Feltpersonell",
-                        Control_status = "Kontrollstatus",
-                        Assessment = "Vurdering",
-                        Location = "Funnsted",
-                        North_original = "Nord (opprinnelig)",
-                        East_Original = "Øst (opprinnelig)",
-                        North_UTM33 = "Nord (UTM33/SWEREF99 TM)",
-                        East_UTM33 = "Øst (UTM33/SWEREF99 TM)",
-                        North_RT90 = "Nord (RT90)",
-                        East_RT90 = "Øst (RT90)",
-                        Coordinate_system = "Koordinatsystem",
-                        Site_quality = "Stedkvalitet",
-                        Approved_date = "Godkjentdato",
-                        Outcome = "Utfall", 
-                        Counted_off_against_decision = "Regnes av mot vedtak", 
-                        Approved_by = "Godkjent av",
-                        Sensitivity = "Følsomhet",
-                        Release_Date = "Frigivelsesdato", 
-                        Id = "Individ", 
-                        SVAID = "SVAID",
-                        Lansstyrelsen_number = "Länsstyrelsens nr",
-                        Last_saved_by = "Sist lagret av",
-                        Last_saved =  "Sist lagret dato",
-                        Municipality_number =  "Kommunenummer",
-                        Municipality = "Kommune", 
-                        County_number = "Fylkenummer",
-                        County = "Fylke"))) %>%
-  ##-- Add some columns
-  dplyr::mutate( 
-    ##-- Add "Country" column
-    Country_sample = substrRight(County, 3),
-    ##-- Change date format
-    Date = as.POSIXct(strptime(Date, "%Y-%m-%d")),
-    ##-- Extract year
-    Year = as.numeric(format(Date,"%Y")),
-    ##-- Extract month
-    Month = as.numeric(format(Date,"%m"))) %>%
-  ##-- Filter data
-  filter(.,
-         ##-- Filter to the focal species
-         Species %in% "Bjørn",
-         ##-- Filter out samples with 
-         Year %in% years) 
+    rename(., any_of(renamingList)) %>%
+    ##-- Add some columns
+    dplyr::mutate( 
+      ##-- Add "Country" column
+      Country_sample = substrRight(County, 3),
+      ##-- Change date format
+      Date = as.POSIXct(strptime(Date, "%Y-%m-%d")),
+      ##-- Extract year
+      Year = as.numeric(format(Date,"%Y")),
+      ##-- Extract month
+      Month = as.numeric(format(Date,"%m"))) %>%
+    ##-- Filter data
+    filter(.,
+           ##-- Filter to the focal species
+           Species %in% "Bjørn",
+           ##-- Filter out samples with 
+           Year %in% years) 
   
   ##############################################################################
   numDetDR <- nrow(DR)                ## NUmber of DEAD RECOVERIES in Rovbase
