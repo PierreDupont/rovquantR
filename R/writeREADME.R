8551a30a2fa8c28a14cdaff8358dc5393f7d8610
PierreDupont <pierre.dup@live.fr>
2026-04-23 12:30:56 +0200

    Fix path to README

--- R/writeREADME.R
+++ R/writeREADME.R
@@ -43,8 +43,7 @@ writeREADME <- function(
   
   ##-- Find the template for the README.rmd file
   if(is.null(template)) {
-    template_path <-"C:/My_documents/rovquantR/inst/rmd/template-README.txt"
-      #system.file("rmd", "template-README", package = "rovquantR")
+    template_path <- system.file("rmd", "template-README", package = "rovquantR")
     if(!file.exists(template_path)) {
       stop("Can not find the template for the README file.\n You must provide the path to the README template through the \"Rmd_template\" argument.")
     }
